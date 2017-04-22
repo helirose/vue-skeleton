1414090738f04baec3a049b26780ca50f7455877
Charlotte <creative.flare@googlemail.com>
2017-04-22 17:58:00 +0100

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  // This is the "main" file which should include all other modules
+  entry: './lib/js/main.js',
+  // Where should the compiled file go?
+  output: {
+    // To the `dist` folder
+    path: './lib/dist',
+    // With the filename `build.js` so it's dist/build.js
+    filename: 'build.js'
+  },
+  resolve: {
+    alias: {
+      vue: 'vue/dist/vue.js'
+    }
+  },
+  module: {
+    // Special compilation rules
+    loaders: [
+      {
+        // Ask webpack to check: If this file ends with .js, then apply some transforms
+        test: /\.js$/,
+        // Transform it with babel
+        loader: 'babel',
+        // don't transform node_modules folder (which don't need to be compiled)
+        exclude: /node_modules/
+      }
+    ]
+  }
+}
